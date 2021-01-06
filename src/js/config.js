2608b6a2505039b2f290d3b89179ea09990d6a8b
fidansuleymanova <48766975+fidansuleymanova@users.noreply.github.com>
2021-01-06 22:26:16 +0400

    first

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a492946c-80da-44ea-ab25-0d3ce117af0a';
+export const MODAL_CLOSE_SEC = 2.5;
